3ff58c37d5c6880f1b1b5d051101debffb5ad379
Shrey K <srkkadia@gmail.com>
2024-06-18 14:54:56 +0530

    Basics

---
+++ Basics/Basic.java
@@ -0,0 +1,10 @@
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("Parv Rajput is learning Java");
+    }
+}
+
+/*
+To Compile  : javac Basic.java
+To Run      : java Basic
+*/
