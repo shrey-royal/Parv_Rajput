cf9bea13856a7915740dc3a2efae9906693733ec
Shrey K <srkkadia@gmail.com>
2024-06-22 12:54:54 +0530

    ScanData

--- Basics/Basic.java
+++ Basics/Basic.java
@@ -1,6 +1,14 @@
 public class Basic {
     public static void main(String[] args) {
-        System.out.println("Parv Rajput is learning Java");
+        // System.out.println("Parv Rajput is learning Java");
+
+        // String name = "Parv";
+        // float marks = 91.4456f;
+
+        // System.out.println("My name is " + name + ", and I got " + marks + "% in my last grade");
+
+        // System.out.printf("My name is %s and I got %.2f%% in my last grade", name, marks);
+
     }
 }
 
