132fe570c8ced43dbec7af79e052fd65b1d87a5c
Shrey K <srkkadia@gmail.com>
2024-06-23 14:02:58 +0530

    Operators (Conditional, Assignment, Ternary)

--- Basics/Operators.java
+++ Basics/Operators.java
@@ -9,8 +9,24 @@ public class Operators {
         a = sc.nextInt();
         b = sc.nextInt();
 
-        System.out.println(a + " + " + b + " = " + (a+b));
-        System.out.println(a + " - " + b + " = " + (a-b));
+        // System.out.println(a + " + " + b + " = " + (a+b));
+        // System.out.println(a + " - " + b + " = " + (a-b));
+
+        // System.out.println(a + " > " + b + " = " + (a>b));
+        // System.out.println(a + " < " + b + " = " + (a<b));
+        // System.out.println(a + " >= " + b + " = " + (a>=b));    // ! a<b
+        // System.out.println(a + " <= " + b + " = " + (a<=b));    // ! a>b
+        // System.out.println(a + " == " + b + " = " + (a==b));
+        // System.out.println(a + " != " + b + " = " + (a!=b));
+
+        // System.out.println(a + ", " + b);
+        // a += b; // a = a + b;
+        // System.out.println(a + ", " + b);
+        // b -= a;
+        // System.out.println(a + ", " + b);
+        // System.out.println(b-=a);
+
+        System.out.println((a>b)?"A>B":"A<B");
 
         sc.close();
     }
@@ -20,11 +36,11 @@ public class Operators {
 OPerators:
 
 1. Arithmetic Operators -> +, -, *, / (quotient), % (remainder)
-2. Conditional Operators
-3. Assignment Operators
+2. Conditional Operators (return boolean) -> <, >, <=, >=, ==, !=
+3. Assignment Operators -> =, +=, -=, *=, /=, %=
 4. Increment/Decrement Operators
 5. Logical Operators
 6. Bitwise Operators
-7. Ternary Operator 
+7. Ternary Operator -> (condition)?true:false
 
 */
