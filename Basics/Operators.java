4a7306fae9869f52c3bef94cc43d063503e5ef7c
Shrey K <srkkadia@gmail.com>
2024-07-13 13:41:42 +0530

    Logical, Bitiwse Operators

--- Basics/Operators.java
+++ Basics/Operators.java
@@ -26,7 +26,23 @@ public class Operators {
         // System.out.println(a + ", " + b);
         // System.out.println(b-=a);
 
-        System.out.println((a>b)?"A>B":"A<B");
+        // System.out.println((a>b)?"A>B":"A<B");
+
+        // int a = 22;
+        // System.out.println("a = " + a + "\n++a = " + ++a + "\na = " + a);
+        // System.out.println("a = " + a + "\na++ = " + a++ + "\na = " + a);
+
+        // System.out.println("(a<b) && (a>b) : " + ((a<b) && (a>b)));
+        // System.out.println("(a<=b) && (a==b) : " + ((a<=b) && (a==b)));
+        // System.out.println("(a<b) || (a>b) : " + ((a<b) || (a>b)));
+        // System.out.println("(a<=b) || (a==b) : " + ((a<=b) || (a==b)));
+        // System.out.println("!(a<b) : " + !(a<b));
+
+        System.out.println((a & b) + "\n" + (a | b));
+        //45 3
+        //678 677
+        //223 567
+        //1 45
 
         sc.close();
     }
@@ -38,9 +54,9 @@ OPerators:
 1. Arithmetic Operators -> +, -, *, / (quotient), % (remainder)
 2. Conditional Operators (return boolean) -> <, >, <=, >=, ==, !=
 3. Assignment Operators -> =, +=, -=, *=, /=, %=
-4. Increment/Decrement Operators
-5. Logical Operators
-6. Bitwise Operators
+4. Increment/Decrement Operators -> (pre/post) (inc/dec)
+5. Logical Operators -> && (and), ||(or), !(not)
+6. Bitwise Operators -> &, |, ^(xor), <<, >>
 7. Ternary Operator -> (condition)?true:false
 
 */
